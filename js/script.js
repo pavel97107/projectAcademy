1f254a1180c3f6fec76d5b6fd2359ea20ae6f74b
Pavel Siarebrany <pavel97107@gmail.com>
2020-02-18 20:14:35 +0300

    lesson17DOP

--- js/script.js
+++ js/script.js
@@ -1,68 +1,71 @@
-window.addEventListener('DOMContentLoaded', function(){
-'use strict';
-
-    // Timer
-    function countTimer(deadline){
-        let timeHours = document.querySelector('#timer-hours'),
-            timeMinutes = document.querySelector('#timer-minutes'),
-            timeSeconds = document.querySelector('#timer-seconds');
-
-            function getTimeRemaining(){
-            let dateStop = new Date(deadline).getTime(), // крайняя дата
-            dateNow = new Date().getTime(), // текущая дата (getTime()-метод преобразует в милисекунды)
-            timeRemaining = (dateStop - dateNow) / 1000, // конвертируем мили секунды в секунды
-            seconds = Math.floor(timeRemaining % 60), // теперь мы имеем секунды(поулчаем чистые секунды делением на остаток)
-            minutes = Math.floor((timeRemaining / 60) % 60), // теперь мы имеем минуты (вычисляем количество минут в часе!!!)
-            hours = Math.floor(timeRemaining / 60 / 60 % 24), // получили часы
-            day = Math.floor(timeRemaining / 60 / 60 / 24 ); // получили дни
-                return {timeRemaining, hours, minutes, seconds};
-            }
-
-            function updateClock(){
-            let timer = getTimeRemaining();
-            timeHours.textContent = timer.hours;
-            timeMinutes.textContent = timer.minutes;
-            timeSeconds.textContent = timer.seconds;
-
-              if(timer.hours >= 1 && timer.hours <= 9){
-                timeHours.textContent = '0' + timer.hours;
-              }
-              if(timer.minutes >= 1 && timer.minutes<= 9){
-                timeMinutes.textContent = '0' + timer.minutes;
-              }
-              if(timer.seconds  >= 0 && timer.seconds <= 9){
-                timeSeconds.textContent = '0' + timer.seconds;
-              }
-
-                if (timer.timeRemaining < 0) {
-                    let doubleZero = '00';
-                    timeHours.textContent = doubleZero;
-                    timeMinutes.textContent = doubleZero;
-                    timeSeconds.textContent = doubleZero;
-                }
-            }
-            updateClock();
-    }
-    setInterval(countTimer, 100, '19 feb 2020');
-});
-
-// deadline то время до которого функция будет отсчитывать
-
-
-
-let dateNew = new Date();
-
-dateNew.setDate(dateNew.getDate() - 1);
-console.log('dateNew: ', dateNew); // так мы вычислили какой день был вчера
-
-console.log(dateNew.toDateString()); // получаем дату в виде строки
-console.log(dateNew.toTimeString()); // получаем время в виде строки
-
-console.log(dateNew.toLocaleDateString('rus')); // получаем дату в виде строки / можем добавить локализацию
-console.log(dateNew.toLocaleTimeString('rus')); // получаем время в виде строки 
-
-
-console.log(dateNew.toISOString('rus').substr(0, 10)); //  получаем время в iso формате оставляем только первые 10 символов
-
-console.log(Date.now()); // количество мл секунд прошедших с 19070 года 1 января
-console.log(Date.parse('24 april 1998')); // количество мл секунд прошедших 1998 года 24 апреля
+document.addEventListener('DOMContentLoaded', function(){
+  'use strict';
+  
+  let day = document.querySelector('.day'),
+      newyear = document.querySelector('.newyear'),
+      getTime = document.querySelector('.getTime');
+  
+      let newDate = new Date();
+  
+      let getHours = newDate.getHours();
+      console.log('getHours: ', getHours);
+  
+      function hours(){
+          if(getHours > 6 && getHours < 10){
+              getTime.textContent = 'Доброе ' + ' утро';
+          } else if (getHours > 10 && getHours < 16){
+              getTime.textContent = 'Добрый ' + ' день';
+          } else if (getHours > 16 && getHours <= 19) {
+              getTime.textContent = 'Добрый ' + ' вечер';
+          } else {
+              getTime.textContent = 'Ложись спать братишка';
+          }
+      }
+      hours();
+      
+      let dayX = newDate.getDay();
+  
+      if(dayX === 1) {
+          dayX = 'Понедельник';
+          day.textContent = day.textContent + " " + dayX;
+      } else if( dayX === 2) {
+          dayX = 'Вторник';
+          day.textContent = day.textContent + " " + dayX;
+      } else if( dayX === 3) {
+          dayX = 'Среда';
+          day.textContent = day.textContent + " " + dayX;
+      } else if( dayX === 4) {       
+          dayX = 'Четверг';
+          day.textContent = day.textContent + " " + dayX;
+      }else if( dayX === 5) {
+          dayX = 'Пятница';
+          day.textContent = day.textContent + " " + dayX;
+      }else if( dayX === 6) {
+          dayX = 'Суббота';
+          day.textContent = day.textContent + " " + dayX;
+      }else if( dayX === 7) {
+          dayX = 'Воскресенье';
+          day.textContent = day.textContent + " " + dayX;
+          }
+          
+  
+      const upTime = function (){
+      let newDate = new Date();
+      let data = newDate.toLocaleTimeString('en');
+      document.querySelector('.timeTwo').innerHTML = data;
+      };
+  
+      setInterval(upTime, 1000);
+  
+  
+      function getDaysNewYear(){
+          let daysYear = new Date('1 jan 2021').getTime();
+          let todayDay = new Date().getTime();
+          let sumDaysYear = (daysYear - todayDay) / 1000;
+          let daysNewYear = Math.floor(sumDaysYear / 60 / 60 / 24);
+          return daysNewYear;
+      }
+      
+      newyear.textContent = newyear.textContent +  ` ${getDaysNewYear()} ` + `дней`;
+      
+  });
