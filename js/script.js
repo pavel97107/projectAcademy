99d690c69de36c430c00dbd25be30d11661146d5
Pavel Siarebrany <pavel97107@gmail.com>
2020-02-19 12:57:28 +0300

    ls18n1

--- js/script.js
+++ js/script.js
@@ -43,5 +43,27 @@ window.addEventListener('DOMContentLoaded', function(){
             }
             updateClock();
     }
-    setInterval(countTimer, 100, '21 feb 2020');
+    setInterval(countTimer, 1000, '21 feb 2020');
+
+    //Меню
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+            const handlerMenu = () => {
+                if(!menu.style.transform || menu.style.transform === `translate(-100%)`){
+                    menu.style.transform = `translate(0)`;
+                } else {
+                    menu.style.transform = `translate(-100%)`;
+                }
+            };
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach((item) => item.addEventListener('click',handlerMenu));
+    };
+
+    toggleMenu();
 });
