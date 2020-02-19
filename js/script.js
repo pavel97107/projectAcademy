807029ff04268a62c77538d4bb2d4712f4f426f4
Pavel Siarebrany <pavel97107@gmail.com>
2020-02-19 16:02:29 +0300

    lesson18

--- js/script.js
+++ js/script.js
@@ -62,6 +62,34 @@ window.addEventListener('DOMContentLoaded', function(){
     };
     toggleMenu();
     //Модульное окно
+
+
+    let popUpContent = document.querySelector('.popup-content');
+    //Всплывашка
+    function popUpShow(){
+       
+        let start = Date.now();
+        const timer = setInterval(function(){
+            let timerRemaining = Date.now() - start;
+            if(timerRemaining >= 250){
+                clearInterval(timer);
+                return;
+            }
+            draw(timerRemaining);
+        }, 2);
+        
+        function draw(timerRemaining){
+            popUpContent.style.left = timerRemaining *2.7 + 'px';
+        }
+        
+        if(window.innerWidth <= 768) {
+            popUpContent.style.left = '30%';
+            clearInterval(timer);
+        }
+
+
+}
+
      const togglePopUp = () => {
         const popUp = document.querySelector('.popup'),
         btnPopUp = document.querySelectorAll('.popup-btn'),
@@ -70,10 +98,12 @@ window.addEventListener('DOMContentLoaded', function(){
         btnPopUp.forEach((elem) => {
             elem.addEventListener('click', () => {
                 popUp.style.display = 'block';
+                popUpShow();
             });
         });
         btnPopUpClose.addEventListener('click', ()=> {
             popUp.style.display = 'none';
+            popUpContent.style.left = '-400px';
         });
 
      };
